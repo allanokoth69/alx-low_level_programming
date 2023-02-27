9db3b91716e03d8893a1172bf00e29e451cec68d
allanokoth69 <allanokoth69@gmail.com>
2023-02-27 05:02:21 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,38 @@
-#import "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main -
- * Return:
+ * main- passwords
+ * Return : 0 success
+ *
  */
-int main(void)
-{
-	return (0);
+#define PASSWORD_LENGTH 8
+
+int main() {
+    char password[PASSWORD_LENGTH + 1];
+    int i, rand_num;
+
+    srand(time(NULL)); // Seed the random number generator with the current time
+
+    printf("Generating a random valid password for 101 Crack Me...\n");
+
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
+        rand_num = rand() % 62; // Generate a random number between 0 and 61
+        if (rand_num < 26) {
+            // Generate a random uppercase letter
+            password[i] = 'A' + rand_num;
+        } else if (rand_num < 52) {
+            // Generate a random lowercase letter
+            password[i] = 'a' + rand_num - 26;
+        } else {
+            // Generate a random digit
+            password[i] = '0' + rand_num - 52;
+        }
+    }
+
+    password[PASSWORD_LENGTH] = '\0'; // Null terminate the password string
+
+    printf("Generated password: %s\n", password);
+
+    return 0;
 }
