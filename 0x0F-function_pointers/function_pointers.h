73fcefe89ea7672bb1c5d93aa1157e1a9eb22fe6
allanokoth69 <allanokoth69@gmail.com>
2023-03-22 05:42:40 -0700

    various assignments

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,18 @@
+
+#ifndef POINTERFUNCS
+#define POINTERFUNCS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+int _putchar(char c);
+#endif /* POINTERFUNCS */
